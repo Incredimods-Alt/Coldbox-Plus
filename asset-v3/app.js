d1ba0f51b2682f690ea22ae3dbe6f795c1cd055a
Desmondxx2000 <157266806+Desmondxx2000@users.noreply.github.com>
2024-07-12 03:02:15 -0400

    Add files via upload

--- asset-v3/app.js
+++ asset-v3/app.js
@@ -1,86 +1,92 @@
-/* This looks like invasion lol - Ters */
+/* 0.5.7 2020-12-27 16:37:09 */
 var app = new function() {
-    this.name = "OldChanic", this.version = "3", this.date = "2022", this.folder = "asset-v3/", this.looptime = 6857, this.bpm = 140, this.totalframe = 452, this.nbpolo = 7, this.nbloopbonus = 1, this.bonusloopA = !0, this.bonusendloopA = !1, this.recmaxloop = 92, this.recminloop = 3, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepicto = "game-picto.png", this.colBck = "#0a190f", this.col0 = "#6EB950", this.col1 = "#41A032", this.col2 = "#2D7D28", this.col3 = "#235023", this.col4 = "#14321E", this.animearray = [{
+    this.name = "Mechanic", this.version = "1", this.date = "2020", this.folder = "asset-v1/", this.looptime = 6857, this.bpm = 140, this.totalframe = 452, this.nbpolo = 7, this.nbloopbonus = 8, this.bonusloopA = !0, this.bonusendloopA = !1, this.recmaxloop = 80, this.recminloop = 2, this.recmintime = Math.round(this.looptime / 1e3) * this.recminloop, this.spritepolo = "polo-sprite.png", this.spritepolotshirt = "polo-sprite-tshirt.png", this.spritepicto = "game-picto.png", this.colBck = "#000000", this.col0 = "#564BB5", this.col1 = "#4A3FA4", this.col2 = "#362C86", this.col3 = "#332184", this.col4 = "#2C1A67", this.animearray = [{
         name: "1_pump",
-        color: "FAB405",
+        color: "DF8020",
         uniqsnd: !0
     }, {
         name: "2_wheel",
-        color: "FAB405",
+        color: "DF8020",
         uniqsnd: !0
     }, {
         name: "3_tiktuk",
-        color: "FAB405",
+        color: "DF8020",
         uniqsnd: !0
     }, {
         name: "4_hammer",
-        color: "FAB405",
+        color: "DF8020",
         uniqsnd: !0
     }, {
         name: "5_snare",
-        color: "FAB405",
+        color: "DF8020",
         uniqsnd: !0
     }, {
         name: "6_bass",
-        color: "23BEE1",
-        uniqsnd: !0
+        color: "8F82FF",
+        uniqsnd: !1
     }, {
         name: "7_cube",
-        color: "23BEE1",
+        color: "8F82FF",
         uniqsnd: !0
     }, {
         name: "8_breather",
-        color: "23BEE1",
+        color: "8F82FF",
         uniqsnd: !0
     }, {
         name: "9_transition",
-        color: "23BEE1",
-        uniqsnd: !0
+        color: "8F82FF",
+        uniqsnd: !1
     }, {
         name: "10_hit",
-        color: "23BEE1",
-        uniqsnd: !0
+        color: "8F82FF",
+        uniqsnd: !1
     }, {
         name: "11_alert",
-        color: "64B92D",
-        uniqsnd: !0
+        color: "20DF20",
+        uniqsnd: !1
     }, {
         name: "12_synth",
-        color: "64B92D",
+        color: "20DF20",
         uniqsnd: !0
     }, {
         name: "13_beep",
-        color: "64B92D",
+        color: "20DF20",
         uniqsnd: !0
     }, {
         name: "14_arpeg",
-        color: "64B92D",
+        color: "20DF20",
         uniqsnd: !0
     }, {
         name: "15_tunbum",
-        color: "64B92D",
+        color: "20DF20",
         uniqsnd: !0
     }, {
         name: "16_mechanic",
-        color: "64B92D",
-        uniqsnd: !0
+        color: "DF2020",
+        uniqsnd: !1
     }, {
         name: "17_struck",
-        color: "64B92D",
-        uniqsnd: !0
+        color: "DF2020",
+        uniqsnd: !1
     }, {
         name: "18_woah",
-        color: "64B92D",
+        color: "DF2020",
         uniqsnd: !0
     }, {
         name: "19_hum",
-        color: "64B92D",
+        color: "DF2020",
         uniqsnd: !0
     }, {
         name: "20_doot",
-        color: "64B92D",
+        color: "DF2020",
         uniqsnd: !0
-    }], this.bonusarray = [], this.unlockerarray = [];
+    }], this.bonusarray = [{
+        name: "Mechanic",
+        src: "v1-b1-mechanic-hb.mp4",
+        code: "1,3,6,12,20",
+        sound: "bonus-mechanic",
+        aspire: "aspire-mechanic"
+    }], this.unlockerarray = [];
     for (var n = 0, o = this.animearray.length; n < o; n++) {
         var i = this.animearray[n].name;
         this.animearray[n].soundA = i + "_a", this.animearray[n].soundB = this.animearray[n].uniqsnd ? i + "_a" : i + "_b", this.animearray[n].anime = i + "-sprite.png", this.animearray[n].animeData = i + ".json"
